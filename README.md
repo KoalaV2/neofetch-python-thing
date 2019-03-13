15afb3789f7e2b750391cbf4d9f312c8fadde28c
KoalaV2 <theo.johanson@gmail.com>
2019-03-13 08:10:00 +0100

    added stuff

---
+++ README.md
@@ -0,0 +1,1 @@
+# Updating the read me soon with all neofetch options. This script exists just beacuse i wanted to make something in python, and yes this could have been easier in bash
---
+++ os.py
@@ -0,0 +1,36 @@
+# This was annoying to make  i dont know why i made this
+#!/ust/bin/env python
+print ("Check out the #README in the dir you cloned this into for all the avalible options and distros \n")
+import subprocess
+call = subprocess.call
+
+fetch = input("What do you want for a fetch? \n")
+
+if fetch == "big":
+    call("neofetch", shell=True)
+elif fetch == "small":
+    call('ufetch')
+elif fetch == "arch":
+    call('neofetch --ascii_distro arch', shell=True)
+elif fetch == "arch cow":
+    call('neofetch --ascii "$(cowsay i use arch btw :D)"', shell=True)
+elif fetch == "rainbow":
+    call('neofetch | lolcat', shell=True)
+elif fetch == "small rainbow":
+    call('ufetch | lolcat', shell=True)
+elif fetch == "gentoo":
+    call('neofetch --ascii_distro gentoo', shell=True)
+elif fetch == "gentoo small":
+    call('neofetch --ascii_distro gentoo_small', shell=True)
+elif fetch == "gentoo rainbow":
+    call('neofetch --ascii_distro gentoo | lolcat', shell=True)
+elif fetch == 'gentoo small rainbow':
+    call('neofetch --ascii_distro gentoo_small | lolcat', shell=True)
+elif fetch == 'nixos':
+    call('neofetch --ascii_distro nixos', shell=True)
+elif fetch == 'nixos small':
+    call('neofetch --ascii_distro nixos_small', shell=True)
+
+else:
+    call('exit', shell=True)
+    print ("Wrong command. Rerun the script.")
